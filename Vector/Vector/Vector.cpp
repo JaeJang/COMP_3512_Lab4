1913b0d19e460ef788da7e1b3769ab1267b0c296
Jae Jang <jaejang5566@gmail.com>
2018-02-07 22:41:56 -0800

    getters and setters

--- Vector/Vector/Vector.cpp
+++ Vector/Vector/Vector.cpp
@@ -26,4 +26,48 @@ Vector::Vector(const Vector &rhs)
 //Destructor
 Vector::~Vector() { }
 
+//Get x coordinate
+//RETURN	: x coordinate
+double Vector::get_X()
+{
+	return 0.0;
+}
+
+//Get y coordinate
+//RETURN	: y coordinate
+double Vector::get_Y()
+{
+	return 0.0;
+}
+
+//Get x coordinate
+//RETURN	: z coordinate
+double Vector::get_Z()
+{
+	return 0.0;
+}
+
+//Set x coordinate
+//PRE	: double parameter
+//POST	: x is set 
+void Vector::set_X(double x)
+{
+	this->x = x;
+}
+
+//Set y coordinate
+//PRE	: double parameter
+//POST	: y is set 
+void Vector::set_Y(double y)
+{
+	this->y = y;
+}
+
+//Set z coordinate
+//PRE	: double parameter
+//POST	: x is set  
+void Vector::set_Z(double z)
+{
+	this->z = z;
+}
 
